ca0a0d6262564300c38d4acfba3c7980db40c95f
Simon Chevalier <sim.chevalier@gmail.com>
2025-06-22 19:41:05 +0200

    CLIENT - responsive navbar + correcting logo

--- workspaces/client/src/app/components/navbar/Navbar.tsx
+++ workspaces/client/src/app/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useAuth } from '@contexts/AuthContext';
-import LoveLetterLogo from '@public/love-letter-logo.png';
+import ShadowNetworkLogo from '@public/shadow_network_logo_horizontal.png';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
@@ -28,13 +28,13 @@ export default function Navbar() {
   };
 
   return (
-    <div className="bg-bg-navbar fixed top-0 z-40 flex w-full flex-row items-center justify-between px-6 py-4 shadow-sm shadow-neutral-950">
+    <div className="bg-bg-navbar fixed top-0 z-40 flex w-full flex-row items-center justify-between px-6 py-2 shadow-sm shadow-neutral-950 sm:py-3 md:py-4">
       <Link href="/">
         <div className="flex flex-row items-center gap-2">
           <Image
-            src={LoveLetterLogo}
-            className="w-18"
-            alt="nqbral-games-logo"
+            src={ShadowNetworkLogo}
+            className="w-12 sm:w-16 md:w-20"
+            alt="shadow-network-logo"
           />
         </div>
       </Link>
@@ -43,7 +43,7 @@ export default function Navbar() {
         <div>
           <Link
             href={`${process.env.NEXT_PUBLIC_WS_NQBRAL_GAMES_URL}/profile/informations`}
-            className="transition-colors hover:text-neutral-300"
+            className="text-xs transition-colors hover:text-neutral-300 sm:text-sm md:text-base"
             target="_blank"
           >
             {userName}
@@ -52,7 +52,7 @@ export default function Navbar() {
       )}
       {isLogged == false && (
         <button
-          className="transition-colors hover:text-neutral-300"
+          className="text-xs transition-colors hover:text-neutral-300 sm:text-sm md:text-base"
           onClick={toLogin}
         >
           Se connecter
