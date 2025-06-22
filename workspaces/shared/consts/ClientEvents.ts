5c11c9ef7aad58f723356cba7874a51cfe91bf8c
Simon Chevalier <sim.chevalier@gmail.com>
2025-06-22 19:45:07 +0200

    SHARED - add gameplay without effect and strategist part two client event

--- workspaces/shared/consts/ClientEvents.ts
+++ workspaces/shared/consts/ClientEvents.ts
@@ -8,6 +8,7 @@ export const CLIENT_EVENTS = {
 
   // Game
   GAME_READY: "client.game.ready",
+  GAME_PLAY_WITHOUT_EFFECT: "client.game.play.without.effect",
   GAME_PLAY_SECRET_OPERATOR: "client.game.play.secret.operator",
   GAME_PLAY_SECURITY_AGENT: "client.game.play.security.agent",
   GAME_PLAY_INFORMANT: "client.game.play.informant",
@@ -15,6 +16,7 @@ export const CLIENT_EVENTS = {
   GAME_PLAY_DISCREET_ASSISTANT: "client.game.play.discreet.assistant",
   GAME_PLAY_UNDERCOVER_AGENT: "client.game.play.undercover.agent",
   GAME_PLAY_STRATEGIST: "client.game.play.strategist",
+  GAME_PLAY_STRATEGIST_PART_TWO: "client.game.play.strategist.part.two",
   GAME_PLAY_DIRECTOR_OF_OPERATIONS: "client.game.play.director.of.operations",
   GAME_PLAY_DIPLOMAT: "client.game.play.diplomat",
   GAME_PLAY_DOUBLE_AGENT: "client.game.play.double.agent",
